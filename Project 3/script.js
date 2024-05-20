b60ec6be8fab97170808aea41a74ad3846897743
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-05-20 12:11:13 +0530

    Delete Project 3 directory

--- Project 3/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 0) {
-        $("#myHeader").fadeOut();
-      } else {
-        $("#myHeader").fadeIn();
-      }
-    });
-  });
